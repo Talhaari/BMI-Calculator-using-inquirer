717b49a3248d5e4cb161e0f65eff72dd7858f600
Talha arif <160534871+Talhaari@users.noreply.github.com>
2024-03-11 00:58:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// BMI CALCULATOR USING INQUIRER PROMPT
+import inquirer from "inquirer";
+const question = [
+    { type: 'number', name: 'weight', message: 'weight(kg):' },
+    { type: 'number', name: 'height', message: 'height(m):' },
+];
+const calculatorBMI = (weight, height) => weight / (height * height);
+const main = async () => {
+    const { weight, height } = await inquirer.prompt(question);
+    console.log(`BMI:${calculatorBMI(weight, height).toFixed(4)}`);
+};
+main();
